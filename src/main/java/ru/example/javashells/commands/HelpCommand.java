cb774e40c4b1f738f7e5a876df51c652143b3e99
aim_41tt <0505mustik@gmail.com>
2024-05-31 15:31:48 +0300

    added command help

---
+++ src/main/java/ru/example/javashells/commands/HelpCommand.java
@@ -0,0 +1,18 @@
+package ru.example.javashells.commands;
+
+import ru.example.javashells.interfaces.Command;
+
+public class HelpCommand implements Command {
+
+	@Override
+	public String getName() {
+		return "help";
+	}
+	
+	@Override
+	public void execute(String[] args) {
+		System.out.print("cd - еремещщение по каталогам  (-) вернутся назад "
+				+ "crdir - создание папки"
+				+ "exit - выход из ос");
+	}
+}
