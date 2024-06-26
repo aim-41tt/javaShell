f20ea0d00d4936ef30be4d7dea9747e564646c3d
aim_41tt <0505mustik@gmail.com>
2024-06-27 01:41:58 +0300

    refactor: Improved Ls command using lambda expressions

--- src/main/java/ru/example/javashells/commands/catalogues/LsCommand.java
+++ src/main/java/ru/example/javashells/commands/catalogues/LsCommand.java
@@ -1,6 +1,8 @@
 package ru.example.javashells.commands.catalogues;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Optional;
 
 import ru.example.javashells.components.managers.DirectoryManager;
 import ru.example.javashells.interfaces.Command;
@@ -22,37 +24,31 @@ public class LsCommand implements Command {
 	public void execute(String[] args) {
 		try {
 
-			File[] files = directoryManager.getCurrentDirectory().listFiles();
+			File[] files = Optional.ofNullable(directoryManager.getCurrentDirectory().listFiles())
+					.orElse(new File[0]);
 
-			if (files.length >= 0) {
+			if (files.length > 0) {
 				System.out.println("Файлы и папки в текущей директории:");
-
 				System.out.println(directoryContentBuilder(files));
 			} else {
 				System.out.println("Файлы и папки не найдены в текущей директории:");
 			}
-		
-		} catch (
 
-		Exception e) {
-			System.out.println(e);
+		} catch (Exception e) {
+			e.getMessage();
 		}
 	}
 
 	private String directoryContentBuilder(File[] files) {
-	    StringBuilder sb = new StringBuilder();
-	    String format = "%-10s %-30s %20s %8.2f КБ%n";
+		String format = "%-10s %-30s %20s %8.2f КБ%n";
 
-	    for (File file : files) {
-	        double lengthFile = file.length() / 1024.0;
-
-	        String type = file.isFile() ? "Файл:" : "Папка:";
-	        
-	        sb.append(String.format(format, type, file.getName(), "размер:", lengthFile));
-	    }
-
-	    return sb.toString();
+		return Arrays.stream(files).map(file -> {
+			double lengthFile = file.length() / 1024.0;
+			String type = file.isFile() ? "Файл:" : "Папка:";
+			return String.format(format, type, file.getName(), "размер:", lengthFile);
+		})
+		.reduce(new StringBuilder(), (sb, str) -> sb.append(str), StringBuilder::append)
+		.toString();
 	}
-
 
 }
