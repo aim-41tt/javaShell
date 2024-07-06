aaf7a16543f857f4d17ba18fa32e540a98afaf6a
aim_41tt <0505mustik@gmail.com>
2024-07-06 18:46:52 +0300

    added a command to view information about the system

---
+++ src/main/java/ru/example/javashells/commands/systemMonitors/MonitorCommand.java
@@ -0,0 +1,98 @@
+package ru.example.javashells.commands.systemMonitors;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.ThreadMXBean;
+import java.util.Arrays;
+
+import ru.example.javashells.interfaces.Command;
+
+public class MonitorCommand implements Command {
+
+    @Override
+    public String getName() {
+        return "monitor";
+    }
+
+    @Override
+    public void execute(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Мониторинг системных ресурсов:\n\n");
+        appendSystemInfo(sb);
+        appendCpuUsage(sb);
+        appendMemoryUsage(sb);
+        appendDiskUsage(sb);
+
+        System.out.println(sb.toString());
+    }
+
+    private void appendSystemInfo(StringBuilder sb) {
+        OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+        ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+        
+        // Информация о системе
+        sb.append("Информация о системе:\n")
+          .append("  ОС: ").append(osBean.getName()).append(", ").append(osBean.getVersion()).append("\n")
+          .append("  Архитектура: ").append(osBean.getArch()).append("\n")
+          .append("  Количество процессоров (ядер): ").append(threadBean.getThreadCount()).append("\n")
+          .append("  Количество потоков: ").append(osBean.getAvailableProcessors()).append("\n\n");
+        
+    }
+
+    private void appendCpuUsage(StringBuilder sb) {
+        OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+
+        double loadAverage = osBean.getSystemLoadAverage();
+
+        sb.append("Использование ЦП:\n")
+          .append("  Средняя нагрузка системы с момента включения:\n")
+          .append("  ").append(formatLoad(loadAverage)).append("\n");
+
+    }
+
+    private void appendMemoryUsage(StringBuilder sb) {
+        Runtime runtime = Runtime.getRuntime();
+        long totalMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+        long maxMemory = runtime.maxMemory();
+        long usedMemory = totalMemory - freeMemory;
+
+        sb.append("Использование памяти:\n")
+          .append("  Использовано памяти: ").append(bytesToMB(usedMemory)).append(" МБ\n")
+          .append("  Свободно памяти: ").append(bytesToMB(freeMemory)).append(" МБ\n")
+          .append("  Общий объем памяти: ").append(bytesToMB(totalMemory)).append(" МБ\n")
+          .append("  Максимальный объем памяти: ").append(bytesToMB(maxMemory)).append(" МБ\n\n");
+    }
+
+    private void appendDiskUsage(StringBuilder sb) {
+        File[] roots = File.listRoots();
+
+        sb.append("Использование диска:\n");
+        Arrays.stream(roots).forEach(root -> {
+            long totalSpace = root.getTotalSpace();
+            long freeSpace = root.getFreeSpace();
+            long usableSpace = root.getUsableSpace();
+
+            sb.append("  Корень файловой системы: ").append(root.getAbsolutePath()).append("\n")
+              .append("    Общий объем диска: ").append(bytesToGB(totalSpace)).append(" ГБ\n")
+              .append("    Свободное дисковое пространство: ").append(bytesToGB(freeSpace)).append(" ГБ\n")
+              .append("    Доступное дисковое пространство: ").append(bytesToGB(usableSpace)).append(" ГБ\n");
+        });
+        sb.append("\n");
+    }
+
+    private long bytesToMB(long bytes) {
+        return bytes / (1024 * 1024);
+    }
+
+    private long bytesToGB(long bytes) {
+        return bytes / (1024 * 1024 * 1024);
+    }
+    
+    private String formatLoad(double loadAverage) {
+        return (loadAverage < 0) ? "информация недоступна" : String.format("%.2f", loadAverage);
+    }
+    
+}
--- src/main/java/ru/example/javashells/configs/CommandConfig.java
+++ src/main/java/ru/example/javashells/configs/CommandConfig.java
@@ -13,6 +13,7 @@ import ru.example.javashells.commands.catalogues.CrdirCommand;
 import ru.example.javashells.commands.catalogues.DelCommand;
 import ru.example.javashells.commands.catalogues.LsCommand;
 import ru.example.javashells.commands.catalogues.RenameCommand;
+import ru.example.javashells.commands.systemMonitors.MonitorCommand;
 import ru.example.javashells.components.managers.DirectoryManager;
 import ru.example.javashells.interfaces.Command;
 
@@ -39,11 +40,12 @@ public class CommandConfig {
 		registerCommand(commandMap, new DelCommand(directoryManager));
 		registerCommand(commandMap, new CopyCommand(directoryManager));
 		registerCommand(commandMap, new RenameCommand(directoryManager));
-
-		commandMap.put("exit", new ExitCommand());
-		commandMap.put("help", new HelpCommand());
-		commandMap.put("cls", new ClsCommand());
 		
+		registerCommand(commandMap, new MonitorCommand());
+
+		registerCommand(commandMap, new ClsCommand());
+		registerCommand(commandMap,new ExitCommand());
+		registerCommand(commandMap, new HelpCommand());
 		return commandMap;
 	}
 
